4edf0fef630fb141ad9204f2e959ef996af9cdbb
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2018-02-15 23:05:52 +0300

    Deleted library dependency, added debug and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fps Booster Light
+
+### Blocking spam with abnormals from another players without QoL loss.
+
+Light version for Kasea's Fps booster without bloat code.
+
+### Differences:
+
+* Easy installation without `library` dependency. 
+
+* Debug support
--- index.js
+++ index.js
@@ -1,33 +1,71 @@
-var Library
-for (let md of ["library", "library-master"]) {
-    try {
-       Library = require(md);
-       break
-    }
-    catch (err) {
-        console.error(err+"[FPS Booster] ERROR! Can't find dependency library. Stopped!");
-        process.exit()
-    }
-}
 const abn = require('./abnormals');
+const debug = false;
 
 class fpsBooster {
     constructor(dispatch) {
+        this.gameId;
+        this.players = {};
+        this.vehicleEx = null;
 
-        const library = Library(dispatch);
 
-        function sAbnormalityApply(e) {
-            let gameId = e.target.toString();
-            if (library.entity.players[gameId] && !library.player.isMe(e.target) && !abn[e.id]) return false;
-        }
-        dispatch.hook('S_ABNORMALITY_BEGIN', 2, { order: 99999999999 }, sAbnormalityApply);
-        dispatch.hook('S_ABNORMALITY_REFRESH', 1, { order: 99999999999 }, sAbnormalityApply);
+        dispatch.hook('S_LOAD_TOPO', 'raw', () => {
+            this.vehicleEx = null;
+            this.players = {};
+            if (debug) console.log('S_LOAD_TOPO');
+        });
 
-        function sAbnormalityEnd(e) {
-            let gameId = e.target.toString();
-            if (library.entity.players[gameId] && !library.player.isMe(e.target) && !abn[e.id]) return false;
-        }
-        dispatch.hook('S_ABNORMALITY_END', 1, { order: 99999999999 }, sAbnormalityEnd);
+        dispatch.hook('S_ABNORMALITY_BEGIN', 2, { order: 1000 }, (event) => {
+            let res = this.players[event.target.toString()] && !this.isMe(event.target) && !abn[event.id]
+            if (debug) console.log(`S_ABNORMALITY_BEGIN for ${event.target} with id ${event.id}  blocked ${res}`);
+            if (res) return false;
+
+        });
+        dispatch.hook('S_ABNORMALITY_REFRESH', 1, { order: 1000 }, (event) => {
+            let res = this.players[event.target.toString()] && !this.isMe(event.target) && !abn[event.id]
+            if (debug) console.log(`S_ABNORMALITY_REFRESH for ${event.target} with id ${event.id} blocked ${res}`);
+            if (res) return false;
+
+        });
+
+        dispatch.hook('S_ABNORMALITY_END', 1, { order: 1000 }, (event) => {
+            let res = this.players[event.target.toString()] && !this.isMe(event.target) && !abn[event.id]
+            if (debug) console.log(`S_ABNORMALITY_END for ${event.target} with id ${event.id}  blocked ${res}`);
+            if (res) return false;
+
+        });
+
+        dispatch.hook('S_LOGIN', 9, (event) => {
+            this.gameId = event.gameId;
+        });
+
+        dispatch.hook('S_SPAWN_USER', 11, (event) => {
+            if (debug) console.log(`S_SPAWN_USER for ${event.gameId}`);
+            if (!this.players[event.gameId.toString()]) {
+                this.players[event.gameId.toString()] = true;
+                if (debug) console.log('added')
+            }
+        });
+
+        dispatch.hook('S_DESPAWN_USER', 3, (event) => {
+            if (debug) console.log(`S_DESPAWN_USER for ${event.gameId}`);
+            if (this.players[event.gameId.toString()]) {
+                delete this.players[event.gameId.toString()]
+                if (debug) console.log('removed')
+            }
+        });
+
+        dispatch.hook('S_MOUNT_VEHICLE_EX', 1, event => {
+            if (event.target.equals(this.gameId)) this.vehicleEx = event.vehicle
+        });
+
+        dispatch.hook('S_UNMOUNT_VEHICLE_EX', 1, event => {
+            if (event.target.equals(this.gameId)) this.vehicleEx = null
+        });
+    }
+
+    //Check gameId(cid)
+    isMe(id) {
+        return this.gameId.equals(id) || this.vehicleEx && this.vehicleEx.equals(id)
     }
 }
 
