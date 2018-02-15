96ea4ad033d2b55c1e9e1906c1dec7d621650ba1
Kasea <1997xeela@gmail.com>
2018-02-15 14:14:15 +0100

    More fps :eyes:

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const Library = require('library');
+try{
+    const Library = require('library');
+}catch(e) {
+    console.log("ERROR! Please download dependency, library.");
+    process.exit();
+}
 const buyPRTho = require('./buyPRTho');
 
 class fpsBoosterNowGoBuyPingRemover{
@@ -7,14 +12,14 @@ class fpsBoosterNowGoBuyPingRemover{
 
         function sAbnormalityApply(e) {
             let gameId = e.target.toString();
-            if(library.entity.players[gameId] && !library.player.isMe(e.target) && buyPRTho[e.id]) return false;
+            if(library.entity.players[gameId] && !library.player.isMe(e.target) && !buyPRTho[e.id]) return false;
         }
         dispatch.hook('S_ABNORMALITY_BEGIN', 2, {order: 99999999999}, sAbnormalityApply);
         dispatch.hook('S_ABNORMALITY_REFRESH', 1, {order: 99999999999}, sAbnormalityApply);
 
         function sAbnormalityEnd(e) {
             let gameId = e.target.toString();
-            if(library.entity.players[gameId] && !library.player.isMe(e.target) && buyPRTho[e.id]) return false;
+            if(library.entity.players[gameId] && !library.player.isMe(e.target) && !buyPRTho[e.id]) return false;
         }
         dispatch.hook('S_ABNORMALITY_END', 1, {order: 99999999999}, sAbnormalityEnd);
     }
