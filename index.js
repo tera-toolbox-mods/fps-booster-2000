3c8f314fdbe45a735d72af37453b17680354de18
Kasea <1997xeela@gmail.com>
2018-04-08 08:03:29 +0200

    added command

--- index.js
+++ index.js
@@ -12,20 +12,17 @@ try{
 class fpsBoosterNowGoBuyPingRemover{
     constructor(dispatch) {
         const library = Library(dispatch);
-        const IfYouBuyPrRightNowIllGibYouSomeUwU = (e) => !library.entity.players[e.target.toString()];
+        let enabled = true;
+        const IfYouBuyPrRightNowIllGibYouSomeUwU = (e) => !(library.entity.players[e.target.toString()] && enabled);
+
+        library.cmd.add('fb', ()=> {
+            enabled = !enabled;
+            library.cmd.message(`FPS-Booster enabled: ${enabled.toString()}`);
+        });
 
         dispatch.hook('S_ABNORMALITY_BEGIN', 2, {order: 99999999999}, IfYouBuyPrRightNowIllGibYouSomeUwU);
         dispatch.hook('S_ABNORMALITY_REFRESH', 1, {order: 99999999999}, IfYouBuyPrRightNowIllGibYouSomeUwU);
         dispatch.hook('S_ABNORMALITY_END', 1, {order: 99999999999}, IfYouBuyPrRightNowIllGibYouSomeUwU);
-
-        /*dispatch.hook('S_ABNORMALITY_BEGIN', 2, {order: 9999999999}, (e) => { console.log(`started abnormality: ${e.id}`) });
-        dispatch.hook('S_ABNORMALITY_REFRESH', 1, {order: 9999999999}, (e) => { console.log(`refreshed abnormality: ${e.id}`) });
-        dispatch.hook('S_ABNORMALITY_END', 1, {order: 9999999999}, (e) => { console.log(`ended abnormality: ${e.id}`) });
-
-        dispatch.hook('S_ABNORMALITY_BEGIN', 2, {order: 999999999999}, (e) => { console.log(`Allowed start abnormality: ${e.id}`) });
-        dispatch.hook('S_ABNORMALITY_REFRESH', 1, {order: 999999999999}, (e) => { console.log(`Allowed refresh abnormality: ${e.id}`) });
-        dispatch.hook('S_ABNORMALITY_END', 1, {order: 999999999999}, (e) => { console.log(`Allowed end abnormality: ${e.id}`) });*/
-
     }
 }
 
