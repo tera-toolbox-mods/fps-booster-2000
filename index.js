5563de0d2fb6910e08de516f5e5c14c6adc2697d
Kasea <1997xeela@gmail.com>
2018-12-08 17:33:17 +0100

    revamp (errors incoming lmfao have fun kids)

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
-try{
-    var Library = require('library');
-}catch(e) {
-    try {
-        var Library = require('library-master');
-    }catch(e) {
-        console.log("ERROR! Please download dependency, library.");
-        process.exit();
-    }
-}
+const BLOCK_OPTIONS = { order: 10000 };
 
-class fpsBoosterNowGoBuyPingRemover{
+const PACKETS_TO_BLOCK = [
+  'S_RESET_CHARM_STATUS', 
+  'S_PARTY_MEMBER_QUEST_DATA', 
+  'S_UNICAST_FLOATING_CASTLE_INFO',
+  'S_UNICAST_FLOATING_CASTLE_NAMEPLATE',
+  'S_PARTY_MATCH_LINK',
+  'S_UPDATE_ACHIEVEMENT_PROGRESS',
+  'S_ITEM_CUSTOM_STRING',
+  'S_LOAD_ACHIEVEMENT_LIST'
+];
+
+const BLOCK_PACKETS_IF_SAME_AS_PREVIOUS = [
+  'S_VIEW_PARTY_INVITE',
+  'S_PLAYER_CHANGE_ALL_PROF',
+  'S_F2P_PremiumUser_Permission',
+  'S_WEAK_POINT'
+];
+
+class FpsBooster{
     constructor(dispatch) {
-        const library = Library(dispatch);
         let enabled = true;
-        const IfYouBuyPrRightNowIllGibYouSomeUwU = (e) => !(library.entity.players[e.target.toString()] && enabled);
+        let previous_packets = {};
+      
+        const library = dispatch.require.library;
+        
+        function BLOCK_ABNORMALITY_FROM_PLAYERS(e) {
+            return !(enabled && library.entity.players[e.target.toString()]);
+        }
+        
+        function BLOCK_THIS_PACKET() {
+            return !enabled;
+        }
+        
+        const BLOCK_IF_SAME_AS_PREVIOUS = (name, code, data) => {
+            let ret = !(enabled && previous_packets[name] === data);
+            previous_packets[name] = data;
+            return ret;
+        };
 
-        library.cmd.add('fb', ()=> {
+        dispatch.command.add('fb', ()=> {
             enabled = !enabled;
-            library.cmd.message(`FPS-Booster enabled: ${enabled.toString()}`);
+            dispatch.command.message(`FPS-Booster enabled: ${enabled.toString()}`);
         });
 
-        dispatch.hook('S_ABNORMALITY_BEGIN', 2, {order: 99999999999}, IfYouBuyPrRightNowIllGibYouSomeUwU);
-        dispatch.hook('S_ABNORMALITY_REFRESH', 1, {order: 99999999999}, IfYouBuyPrRightNowIllGibYouSomeUwU);
-        dispatch.hook('S_ABNORMALITY_END', 1, {order: 99999999999}, IfYouBuyPrRightNowIllGibYouSomeUwU);
+        dispatch.hook('S_ABNORMALITY_BEGIN', 3, BLOCK_OPTIONS, BLOCK_ABNORMALITY_FROM_PLAYERS);
+        dispatch.hook('S_ABNORMALITY_REFRESH', 1, BLOCK_OPTIONS, BLOCK_ABNORMALITY_FROM_PLAYERS);
+        dispatch.hook('S_ABNORMALITY_END', 1, BLOCK_OPTIONS, BLOCK_ABNORMALITY_FROM_PLAYERS);
+        
+        for(let name of PACKETS_TO_BLOCK) dispatch.hook(name, 'raw', BLOCK_OPTIONS, BLOCK_THIS_PACKET);
+        
+        for(let name of BLOCK_PACKETS_IF_SAME_AS_PREVIOUS) dispatch.hook(name, 'raw', BLOCK_OPTIONS, BLOCK_IF_SAME_AS_PREVIOUS.bind(null, name));
     }
 }
 
-module.exports = fpsBoosterNowGoBuyPingRemover;
+module.exports = FpsBooster;
